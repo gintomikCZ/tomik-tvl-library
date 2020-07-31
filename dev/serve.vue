ca54e5a10a68c48e7e5bd92b6862bfd2a2a175ca
tomik <tomas@glabazna.cz>
2020-07-31 22:10:47 +0200

    first commit - vue-sfc-rollup scaffold installed

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import { TomikTvlLibrarySample } from '@/entry';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    TomikTvlLibrarySample,
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <tomik-tvl-library-sample />
+  </div>
+</template>
---
+++ src/lib-components/index.js
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as TomikTvlLibrarySample } from './tomik-tvl-library-sample.vue';
